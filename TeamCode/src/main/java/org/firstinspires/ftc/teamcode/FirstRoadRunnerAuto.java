f4b0279dc2c37e3aba2c98e1b3ac34702c173f23
askryan <askryan557@gmail.com>
2024-11-29 18:22:46 -0600

    Hopefully created all the necessary mechanisms? Also realized that the intake mechanisms are unnecessary

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstRoadRunnerAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstRoadRunnerAuto.java
@@ -6,24 +6,17 @@ import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
-import com.acmerobotics.roadrunner.ParallelAction;
 
 // Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Config
 @Autonomous(name = "GoodAuto", group = "Roadrunner")
+
 public class FirstRoadRunnerAuto {
 
     public class ISlides {
@@ -69,8 +62,7 @@ public class FirstRoadRunnerAuto {
 
                 if (posL < MAX_INTAKE_SLIDE_POS && posR < MAX_INTAKE_SLIDE_POS) {
                     return true;
-                }
-                else {
+                } else {
                     iSlideL.setPower(0);
                     iSlideR.setPower(0);
                     return false;
@@ -87,51 +79,143 @@ public class FirstRoadRunnerAuto {
 
     public class IClaw {
         public Servo iClaw;
+        public double minPos = 0; // needs manual testing II
+        public double maxPos = 1; // needs manual testing II
 
         public IClaw(HardwareMap hardwareMap) {
             iClaw = hardwareMap.get(Servo.class, "iClaw");
-            iClaw.scaleRange(0, 1);
+            iClaw.scaleRange(minPos, maxPos);
         }
 
+        public class OpenIClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                iClaw.setPosition(1);
+                telemetryPacket.put("Intake claw position", maxPos);
+                return false;
+            }
+        }
+
+        public Action openIClaw() {
+            return new OpenIClaw();
+        }
+
+        public class CloseIClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                iClaw.setPosition(0);
+                telemetryPacket.put("Intake claw position", minPos);
+                return false;
+            }
+        }
+
+        public Action closeIClaw() {
+            return new CloseIClaw();
+        }
     }
 
     public class IRotation {
         public Servo iRotation;
+        public double minPos = 0; // needs manual testing II
+        public double maxPos = 1; // needs manual testing II
 
         public IRotation(HardwareMap hardwareMap) {
             iRotation = hardwareMap.get(Servo.class, "iRotation");
-            iRotation.scaleRange(0, 1);
+            iRotation.scaleRange(minPos, maxPos);
         }
+
     }
 
     public class IArm {
         public Servo iArmL, iArmR;
 
+        public double minPosL = 0; // needs manual testing II
+        public double maxPosL = 1; // needs manual testing II
+
+        public double minPosR = 0; // needs manual testing II
+        public double maxPosR = 1; // needs manual testing II
+
         public IArm(HardwareMap hardwareMap) {
             iArmL = hardwareMap.get(Servo.class, "L1");
             iArmR = hardwareMap.get(Servo.class, "R1");
-            iArmL.scaleRange(0, 1);
-            iArmR.scaleRange(0, 1);
+            iArmL.scaleRange(minPosL, maxPosL);
+            iArmR.scaleRange(minPosR, maxPosR);
         }
 
     }
 
     public class OArm {
         public Servo oArm;
+        public double minPos = 0; // needs manual testing II
+        public double maxPos = 1; // needs manual testing II
 
         public OArm(HardwareMap hardwareMap) {
             oArm = hardwareMap.get(Servo.class, "oArm");
-            oArm.scaleRange(0, 1);
+            oArm.scaleRange(minPos, maxPos);
+        }
+
+        public class ExtendOArm implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                oArm.setPosition(1);
+                telemetryPacket.put("Outtake claw position", maxPos);
+                return false;
+            }
+        }
+
+        public Action extendOArm() {
+            return new ExtendOArm();
+        }
+
+        public class RetractOArm implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                oArm.setPosition(0);
+                telemetryPacket.put("Outtake claw position", minPos);
+                return false;
+            }
+        }
+
+        public Action retractOArm() {
+            return new RetractOArm();
         }
 
     }
 
     public class OClaw {
         public Servo oClaw;
+        public double minPos = 0; // needs manual testing II
+        public double maxPos = 1; // needs manual testing II
 
         public OClaw(HardwareMap hardwareMap) {
             oClaw = hardwareMap.get(Servo.class, "oClaw");
-            oClaw.scaleRange(0, 1);
+            oClaw.scaleRange(minPos, maxPos);
+        }
+
+        public class OpenOClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                oClaw.setPosition(1);
+                telemetryPacket.put("Outtake claw position", maxPos);
+                return false;
+            }
+        }
+
+        public Action openOClaw() {
+            return new OpenOClaw();
+        }
+
+        public class CloseOClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                oClaw.setPosition(0);
+                telemetryPacket.put("Outtake claw position", minPos);
+                return false;
+            }
+        }
+
+        public Action closeOClaw() {
+            return new CloseOClaw();
         }
     }
 
@@ -154,6 +238,146 @@ public class FirstRoadRunnerAuto {
             oSlideR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             oSlideR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
-    }
 
+        public class OSlidesBottom implements Action {
+
+            public boolean initialized = false;
+            public static final int BOTTOM_OUTTAKE_SLIDE_POS = 0;
+            public static final double OUTTAKE_SLIDE_POWER = 0.5;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
+                if (!initialized) {
+                    oSlideL.setPower(-1 * OUTTAKE_SLIDE_POWER);
+                    oSlideR.setPower(-1 * OUTTAKE_SLIDE_POWER);
+                    initialized = !initialized;
+                }
+
+                int posL = oSlideL.getCurrentPosition();
+                int posR = oSlideR.getCurrentPosition();
+
+                telemetryPacket.put("Left outtake slide position", posL);
+                telemetryPacket.put("Right outtake slide position", posR);
+
+                if (posL > BOTTOM_OUTTAKE_SLIDE_POS && posR > BOTTOM_OUTTAKE_SLIDE_POS) {
+                    return true;
+                } else {
+                    oSlideL.setPower(0);
+                    oSlideR.setPower(0);
+                    return false;
+                }
+            }
+        }
+
+        public Action oSlidesBottom() {
+            return new OSlidesBottom();
+        }
+
+        public class ExtendOSlidesHBar implements Action {
+
+            public boolean initialized = false;
+            public static final int HBAR_OUTTAKE_SLIDE_POS = 1500;
+            public static final double OUTTAKE_SLIDE_POWER = 0.5;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
+                if (!initialized) {
+                    oSlideL.setPower(OUTTAKE_SLIDE_POWER);
+                    oSlideR.setPower(OUTTAKE_SLIDE_POWER);
+                    initialized = !initialized;
+                }
+
+                int posL = oSlideL.getCurrentPosition();
+                int posR = oSlideR.getCurrentPosition();
+
+                telemetryPacket.put("Left outtake slide position", posL);
+                telemetryPacket.put("Right outtake slide position", posR);
+
+                if (posL < HBAR_OUTTAKE_SLIDE_POS && posR < HBAR_OUTTAKE_SLIDE_POS) {
+                    return true;
+                } else {
+                    oSlideL.setPower(0);
+                    oSlideR.setPower(0);
+                    return false;
+                }
+            }
+        }
+
+        public Action extendOSlidesHBar() {
+            return new ExtendOSlidesHBar();
+        }
+
+        public class LowerOSlidesToHook implements Action {
+
+            public boolean initialized = false;
+            public static final int HBAR_OUTTAKE_SLIDE_POS = 1400;
+            public static final double OUTTAKE_SLIDE_POWER = 0.7;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
+                if (!initialized) {
+                    oSlideL.setPower(-1 * OUTTAKE_SLIDE_POWER);
+                    oSlideR.setPower(-1 * OUTTAKE_SLIDE_POWER);
+                    initialized = !initialized;
+                }
+
+                int posL = oSlideL.getCurrentPosition();
+                int posR = oSlideR.getCurrentPosition();
+
+                telemetryPacket.put("Left outtake slide position", posL);
+                telemetryPacket.put("Right outtake slide position", posR);
+
+                if (posL > HBAR_OUTTAKE_SLIDE_POS || posR > HBAR_OUTTAKE_SLIDE_POS) {
+                    return true;
+                } else {
+                    oSlideL.setPower(0);
+                    oSlideR.setPower(0);
+                    return false;
+                }
+            }
+        }
+
+        public Action lowerOSlidesToHook() {
+            return new LowerOSlidesToHook();
+        }
+
+        public class ExtendOSlidesPickup implements Action {
+
+            public boolean initialized = false;
+            public static final int HBAR_OUTTAKE_SLIDE_POS = 800;
+            public static final double OUTTAKE_SLIDE_POWER = 0.5;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
+                if (!initialized) {
+                    oSlideL.setPower(OUTTAKE_SLIDE_POWER);
+                    oSlideR.setPower(OUTTAKE_SLIDE_POWER);
+                    initialized = !initialized;
+                }
+
+                int posL = oSlideL.getCurrentPosition();
+                int posR = oSlideR.getCurrentPosition();
+
+                telemetryPacket.put("Left outtake slide position", posL);
+                telemetryPacket.put("Right outtake slide position", posR);
+
+                if (posL < HBAR_OUTTAKE_SLIDE_POS && posR < HBAR_OUTTAKE_SLIDE_POS) {
+                    return true;
+                } else {
+                    oSlideL.setPower(0);
+                    oSlideR.setPower(0);
+                    return false;
+                }
+            }
+        }
+
+        public Action extendOSlidesPickup() {
+            return new ExtendOSlidesPickup();
+        }
+
+    }
 }
